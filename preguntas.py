9fbba0af0a7013d9bdc1b5c99bab0c3fbe3120dd
aymonsalver <aymonsalver@unal.edu.co>
2022-11-25 12:18:28 -0500

    P7_Init

--- preguntas.py
+++ preguntas.py
@@ -237,7 +237,21 @@ def pregunta_07():
     ]
 
     """
-    return
+    data = open("data.csv", "r").readlines()
+    data = [row[0:-1] for row in data]
+    data = [row.split() for row in data]
+    data = [[row[1], row[0]] for row in data]
+    numeros = sorted(set([row[0] for row in data]))
+    diccionario = {}
+    respuesta = []
+    for numero in numeros:
+        diccionario[numero] = []
+    for numero in numeros:
+        for row in data:
+            if row[0] == numero:
+                diccionario[numero].append(row[1])
+        respuesta.append((int(numero) , diccionario[numero] ))
+    return respuesta
 
 
 def pregunta_08():
